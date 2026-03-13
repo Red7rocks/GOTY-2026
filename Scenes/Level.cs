66e24a9708f13c8679330f98d1d8e9fb39b64acb
Austin Burns <burnsaj137@gmail.com>
2026-03-12 23:21:40 -0400

    Getting trailing characters working

--- Scenes/Level.cs
+++ Scenes/Level.cs
@@ -5,26 +5,64 @@ using System.Collections.Generic;
 public partial class Level : Node2D
 {
 	PackedScene cowboyScene = ResourceLoader.Load<PackedScene>("res://Scenes/cowboy.tscn");
-	CharacterBody2D cowboy;
+	PackedScene alchemistScene = ResourceLoader.Load<PackedScene>("res://Scenes/alchemist.tscn");
+	PackedScene mageScene = ResourceLoader.Load<PackedScene>("res://Scenes/mage.tscn");
+	PackedScene tankScene = ResourceLoader.Load<PackedScene>("res://Scenes/tank.tscn");
+
 	List<CharacterBody2D> party = new List<CharacterBody2D>();
-	public int speed = 400;
-	
-	public void readInput()
+	List<Vector2> positions = new List<Vector2>();
+	Vector2 lastRecordedPosition;
+	int followSpacing = 15;
+	int playerSpeed = 400;
+
+	void MoveLeader()
 	{
-		Vector2 direction = Input.GetVector("a", "d", "w", "s");
-		foreach(CharacterBody2D character in party){
-			character.Velocity = direction * speed;
-			character.MoveAndSlide();
+		Vector2 direction = Input.GetVector("a", "d", "w", "s");	//Grab input from WASD keys 
+		party[0].Velocity = direction * playerSpeed;
+		party[0].MoveAndSlide();
+	}
+	void recordPosition()
+	{
+		if (positions.Count == 0 || party[0].GlobalPosition.DistanceTo(lastRecordedPosition) > 0)
+		{
+			positions.Insert(0, party[0].GlobalPosition);		//Record current position if no positions have been recorded yet,
+			lastRecordedPosition = party[0].GlobalPosition;		//or if distance from last recorded position is non-zero
 		}
+		int maxHistory = followSpacing * party.Count;		//We give ourselves as many position points in our vector as there are spaces between players
+		if (positions.Count > maxHistory)
+		{
+			positions.RemoveAt(positions.Count - 1);			//Memory management so vector doesn't grow infinitely
+		}
+	}
+	void MoveFollowers()
+	{
+		for (int i = 1; i < party.Count; i++)
+		{
+			int index = i * followSpacing;		//Setting index of current player to where leading player was 'followingSpacing' positions ago
+			if (index < positions.Count)		//Make sure position exists. > Count would be outside of the array
+			{
+				Vector2 target = positions[index];										//Lerp = Linear interpolation. smoothly transitions position from 
+				party[i].GlobalPosition = party[i].GlobalPosition.Lerp(target, 0.25f);	//current position to target position at a rate of 0.25 per tick
+			}
+		}
+	}
+	void CreateCharacter(PackedScene scene)
+	{
+		CharacterBody2D newScene = scene.Instantiate<CharacterBody2D>();
+		GetNode<Node2D>("Party").AddChild(newScene);
+		party.Add(newScene);
 	}
 	public override void _Ready()
 	{
-		cowboy = cowboyScene.Instantiate<CharacterBody2D>();
-		AddChild(cowboy);
-		party.Add(cowboy);
+		CreateCharacter(cowboyScene);
+		CreateCharacter(alchemistScene);
+		CreateCharacter(mageScene);
+		CreateCharacter(tankScene);
 	}
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
-		readInput();
+		MoveLeader();
+		recordPosition();
+		MoveFollowers();
 	}
 }
