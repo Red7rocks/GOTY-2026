20017e55c35dbc2f3bafd559fdf62b27310f8962
Austin Burns <burnsaj137@gmail.com>
2026-03-14 12:56:15 -0400

    Creating initial battle scene

---
+++ Scenes/Battle.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class Battle : Node2D
+{
+	public override void _Ready()
+	{
+	}
+	public override void _Process(double delta)
+	{
+	}
+}
--- Scenes/Level.cs
+++ Scenes/Level.cs
@@ -3,22 +3,27 @@ using System;
 
 public partial class Level : Node2D
 {
-	PackedScene cowboyScene = GD.Load<PackedScene>("res://Scenes/Classes/cowboy.tscn");
-	PackedScene alchemistScene = GD.Load<PackedScene>("res://Scenes/Classes/alchemist.tscn");
 	PackedScene mageScene = GD.Load<PackedScene>("res://Scenes/Classes/mage.tscn");
 	PackedScene tankScene = GD.Load<PackedScene>("res://Scenes/Classes/tank.tscn");
+	PackedScene cowboyScene = GD.Load<PackedScene>("res://Scenes/Classes/cowboy.tscn");
+	PackedScene alchemistScene = GD.Load<PackedScene>("res://Scenes/Classes/alchemist.tscn");
 	PackedScene saloonScene = GD.Load<PackedScene>("res://Scenes/Buildings/saloon.tscn");
 	PackedScene badguyScene = GD.Load<PackedScene>("res://Scenes/Enemies/bad_guy.tscn");
 
 	Node2D levelParty;		//Container that will spawn all characters in level
 	Node2D levelBuildings;	//Container that will spawn all buildings in the level
-	Node2D levelEnemies;			//Container that will spawn all enemies in the level
+	Node2D levelEnemies;	//Container that will spawn all enemies in the level
 
 	LevelObject saloon;			//Saloon object accessible across functions;
-	Party party;			//Container that holds all player/movement logic
-	LevelObject badguy;
+	LevelObject badguy;			//badguy object accessible across functions;
 
-	public void nearbySaloon(){
+	Party party;			//Container that holds all player/movement logic
+
+	public void nearbyBuilding(){
+		//Prompt player if they want to enter
+		//if they do, change scene
+	}
+	public void notNearbyBuilding(){
 		//Prompt player if they want to enter
 		//if they do, change scene
 	}
@@ -26,28 +31,39 @@ public partial class Level : Node2D
 		//Prompt player if they want to battle
 		//if they do, change scene
 	}
+	public void notNearbyEnemy(){
+		//Prompt player if they want to battle
+		//if they do, change scene
+	}
+	public void addBuilding(LevelObject levOb, Vector2 position){
+		levelBuildings.AddChild(levOb);		//Add Building to the level
+		levOb.Position = position;			//Set Building position
+		levOb.nearby += nearbyBuilding;		//Attach signal to trigger when character approaches building
+		levOb.notNearby += nearbyBuilding;	//Attach signal to trigger when character walks away from building
+	}
+	public void addEnemy(LevelObject levOb, Vector2 position){
+		levelEnemies.AddChild(levOb);		//Add Enemy to level
+		levOb.Position = position;			//Set Enemy position
+		levOb.nearby += nearbyEnemy;		//Attach signal to trigger when character approaches enemy
+		levOb.notNearby += notNearbyEnemy;	//Attach signal to trigger when character walks away from enemy
+	}
 	public override void _Ready()
 	{
-		party = new Party();
-		saloon = saloonScene.Instantiate<LevelObject>();
-		badguy = badguyScene.Instantiate<LevelObject>();
+		party = new Party();		//Instantiate player party
+		saloon = saloonScene.Instantiate<LevelObject>();	//create saloon object that can be added to the level
+		badguy = badguyScene.Instantiate<LevelObject>();	//create badguy object that can be added to the level
 
-		levelParty = GetNode<Node2D>("Party");
-		levelBuildings = GetNode<Node2D>("Buildings");
-		levelEnemies = GetNode<Node2D>("Enemies");
+		levelParty = GetNode<Node2D>("Party");				//Get reference to Party node in level scene
+		levelBuildings = GetNode<Node2D>("Buildings");		//Get reference to Buildings node in level scene
+		levelEnemies = GetNode<Node2D>("Enemies");			//Get reference to Enemies node in level scene
 
-		levelParty.AddChild(party.CreateCharacter(cowboyScene));			//Add a cowboy to the party and the level party in one call
-		levelParty.AddChild(party.CreateCharacter(alchemistScene));			//Add an alchemist to the party and the level party in one call
-		levelParty.AddChild(party.CreateCharacter(mageScene));				//Add a mage to the party and the level party in one call
-		levelParty.AddChild(party.CreateCharacter(tankScene));				//Add a tank to the party and the level party in one call
+		levelParty.AddChild(party.CreateCharacter(cowboyScene));			//Add a cowboy to the party and the level in one call
+		levelParty.AddChild(party.CreateCharacter(alchemistScene));			//Add an alchemist to the party and the level in one call
+		levelParty.AddChild(party.CreateCharacter(mageScene));				//Add a mage to the party and the level in one call
+		levelParty.AddChild(party.CreateCharacter(tankScene));				//Add a tank to the party and the level in one call
 		
-		levelBuildings.AddChild(saloon);			//Spawn Buildings
-		saloon.Position = new Vector2(650, 100);
-		saloon.nearby += nearbySaloon;
-		
-		levelEnemies.AddChild(badguy);				//Spawn Enemies
-		badguy.Position = new Vector2(650, 600);
-		badguy.nearby += nearbyEnemy;
+		addBuilding(saloon,  new Vector2(650, 100));		//Add saloon building at specified position
+		addEnemy(badguy, new Vector2(650, 600));			//Add badguy enemy at specified position
 	}
 	public override void _PhysicsProcess(double delta)
 	{
