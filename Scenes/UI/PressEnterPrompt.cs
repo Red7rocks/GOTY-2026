ab31a9980b13bd57cd05ba814bad271a2647afb8
Austin Burns <burnsaj137@gmail.com>
2026-04-15 18:44:20 -0400

    adding missing comments

--- Scenes/UI/PressEnterPrompt.cs
+++ Scenes/UI/PressEnterPrompt.cs
@@ -5,16 +5,12 @@ public partial class PressEnterPrompt : Control
 {
 	public void FlashObject()
 	{
-		// Ensure Modulate is initialized
-		Modulate = new Color(Modulate, 1); 
-		
-		Tween _tween = CreateTween();
-		// Set loops to 0 for infinite, or a specific number
-		_tween.SetLoops(); 
+		Tween _tween = CreateTween();	//Tweens are objects that allow gradual transitions between numbers
+		_tween.SetLoops(); 	//set loops to default for infinite
 		
 		// Tween alpha from 1 (opaque) to 0 (transparent) and back
-		_tween.TweenProperty(this, "modulate:a", 0.0f, 0.5f);
-		_tween.TweenProperty(this, "modulate:a", 1.0f, 0.5f);
+		_tween.TweenProperty(this, "modulate:a", 0.0f, 1.0f);	//shift opacity to 0 over the coarse of 1.0f seconds
+		_tween.TweenProperty(this, "modulate:a", 1.0f, 1.0f);	//shift opacity to 1 over the coarse of 1.0f seconds
 	}
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
